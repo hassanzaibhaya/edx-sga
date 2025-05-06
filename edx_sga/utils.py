15470fce57b0f179b86bde5bb869962113771313
Hassan Zaib Hayat <hassan.zaib@A006-01450.local>
2025-05-06 15:57:51 +0500

    Update test cases for django52 settings structure

--- edx_sga/utils.py
+++ edx_sga/utils.py
@@ -65,7 +65,7 @@ def get_file_modified_time_utc(file_path):
         # time.tzname returns a 2 element tuple:
         #   (local non-DST timezone, e.g.: 'EST', local DST timezone, e.g.: 'EDT')
         pytz.timezone(time.tzname[0])
-        if settings.DEFAULT_FILE_STORAGE
+        if settings.STORAGES['default']
         == "django.core.files.storage.FileSystemStorage"
         else pytz.utc
     )
